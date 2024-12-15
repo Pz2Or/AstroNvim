d72e594ea746e0f17a18ef099ad9a041a02c0e05
pz2or <pub@ter05.net>
2024-12-15 16:39:05 +0900

    add: github copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    config = function()
+      require("copilot").setup {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+        copilot_node_command = "node",
+      }
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function() require("copilot_cmp").setup() end,
+  },
+}
